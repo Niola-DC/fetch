930ff3cf979ba464aa90d040e06f2bfd91120680
Eniola Fanegan <eniolachinemerem74@gmail.com>
2024-06-28 10:57:42 +0100

    Demo Commit
    Demo Commit of python file to fetch repo

---
+++ try.py
@@ -0,0 +1,24 @@
+#print("Hello")
+#Name="Daniel"
+#age=30
+#print(f"My name is {Name} ,he is{age}years of age and has{Money} ")
+adictionary= {"Name=Vincent","Age"}
+aList = [21, 25, 27]
+aTuple=(21, 25, 27)
+print(aList)
+print(aTuple)
+
+def add(x,y):
+    return x+y
+print(add(3,4))
+
+def sum_Number(x,y):
+    return x-y
+sum=sum_Number(10000,1900000)
+print(sum)
+
+
+def check(x,y):
+    return x-y
+print(check(40,5))
+
